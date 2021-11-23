37eb7483e502f5bdb2d8bd9b523e02623162ed3d
Tim Layton <tmlayton@users.noreply.github.com>
2021-11-23 11:39:03 -0800

    do not run tests automatically

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cron from 'node-cron';
-import { log, checkStock, runTests } from './utils';
+import { log, checkStock } from './utils';
 
 const app = express();
 
@@ -12,10 +12,12 @@ function scheduleJobs() {
     await checkStock();
   });
 
-  cron.schedule('0 0 */1 * * *', async () => {
-    log('Running tests every hour to make sure in stock pages are working...');
-    await runTests();
-  });
+  // Disabling tests to only get a text when stock is detected
+  // This is probably only useful for manual testing
+  // cron.schedule('0 0 */1 * * *', async () => {
+  //   log('Running tests every hour to make sure in stock pages are working...');
+  //   await runTests();
+  // });
 }
 
 app.listen(6969, () => {
